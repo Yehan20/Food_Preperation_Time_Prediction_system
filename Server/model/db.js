a965c4db28ba74c8e6eb18f75d6ed19b9c919aff
Yehan20 <yehansk15@gmail.com>
2022-12-17 18:23:46 +0530

    .

--- Server/model/db.js
+++ Server/model/db.js
@@ -2,10 +2,10 @@ const express = require('express')
 const mysql = require('mysql');
 
 const db=mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"resturant"
+    host:"db4free.net",
+    user:"yehan123",
+    password:"$firedog123",
+    database:"food_labs_db"
 })
 
 module.exports=db;
