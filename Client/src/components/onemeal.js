41c6ae2f96c43198ebe44dbd7f0ee901ea1f889a
Yehan20 <yehansk15@gmail.com>
2022-12-18 11:19:00 +0530

    .

--- Client/src/components/onemeal.js
+++ Client/src/components/onemeal.js
@@ -85,6 +85,7 @@ const SpecificMeal = () => {
 
             }).then((result) => {
                 setLoading(false)
+                console.log(result)
                 console.log(result.data)
                 if (result.statusText === "OK") {
                     console.log("request send");
